0c048a00691b465a6057cf8331dca9cfd6e8057a
jbarinas <jbarinas.softdev@gmail.com>
2021-01-17 16:43:07 +0000

    fix: minor fixes and adding deps to generator

--- generators/app/templates/src/store/reducer.ts
+++ generators/app/templates/src/store/reducer.ts
@@ -1,6 +1,8 @@
 import {AnyAction, combineReducers} from "redux";
 
-const _reducer = combineReducers({});
+const _reducer = combineReducers({
+  x: (state = [], action) => state
+});
 
 export const reducer = (state: any, action: AnyAction) => {
   return _reducer(state, action);
